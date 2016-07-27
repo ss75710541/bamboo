c3e8d2a17db9ffead74f1ac28ac535fad9939ca2
jyliu <75710541@qq.com>
2016-07-27 14:40:47 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ VERSION=$(cat VERSION)
 IMAGE="bamboo-v${VERSION}"
 HOST="catalog.shurenyun.com"
 NAMESPACE="library"
-TAG="omega.v2.5.7"
+TAG="omega.v2.5.9"
 
 docker build --no-cache -t ${HOST}/${NAMESPACE}/${IMAGE}:${TAG} -f Dockerfile .
